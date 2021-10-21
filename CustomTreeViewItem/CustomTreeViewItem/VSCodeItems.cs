3b496093d01878a028c1aaaeb41fbfac9f233f51
qbaker <qbakernc@gmail.com>
2021-10-21 10:09:24 -0400

    almost done

--- CustomTreeViewItem/CustomTreeViewItem/VSCodeItems.cs
+++ CustomTreeViewItem/CustomTreeViewItem/VSCodeItems.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -48,33 +49,79 @@ namespace CustomTreeViewItem
     public class VSCodeItems : TreeViewItem
     {
         private Border border;
+        private ToggleButton expander;
 
-        public ItemCollection MyItems
-        {
-            get
-            {
-                return Items;
-            }
-        }
-    
         static VSCodeItems()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(VSCodeItems), new FrameworkPropertyMetadata(typeof(VSCodeItems)));
         }
-        protected override void AddChild(object value)
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public new double Width
         {
-
+            get;
+            internal set;
         }
- 
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public new double MaxWidth
+        {
+            get;
+            internal set;
+        }
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public new double Height
+        {
+            get;
+            internal set;
+        }
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public new double MaxHeight
+        {
+            get;
+            internal set;
+        }
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public new Thickness Margin
+        {
+            get;
+            internal set;
+        }
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public new Thickness Padding
+        {
+            get;
+            internal set;
+        }
+
+        internal Thickness MarginThickness
+        {
+            get { return (Thickness)GetValue(MarginThicknessProperty); }
+            set { SetValue(MarginThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MarginThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MarginThicknessProperty =
+            DependencyProperty.Register("MarginThickness", typeof(Thickness), typeof(VSCodeItems), new PropertyMetadata(new Thickness(10030, 0, 5, 0)));
+
+            
+        public void AddItem(VSCodeItems newItem)
+        {
+            newItem.MarginThickness  = new Thickness(10010, 0, 5, 0);
+            Items.Add(newItem); 
+        }
         public override void OnApplyTemplate()
-        { 
+        {
+        
             border = (Border)GetTemplateChild("FullBorder");
+            expander = (ToggleButton)GetTemplateChild("Expander");
+            
             border.MouseEnter += Border_MouseEnter;
             border.MouseLeave += Border_MouseLeave;
         }
+
+
         private void Border_MouseLeave(object sender, MouseEventArgs e)
         {
-           
+   
         }
 
         private void Border_MouseEnter(object sender, MouseEventArgs e)
